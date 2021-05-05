294b66f1291cbd9c7fb0580b9ead198df2278a44
Aleno4ka0 <vselenskoyezlo@gmail.com>
2021-05-05 17:06:09 +0400

    FIX

--- script.js
+++ script.js
@@ -3,50 +3,50 @@ function illidanSay(){
 }
 
 function ioggSay(){
-  new Audio('/song/iogg.mp3').play()
+  new Audio('./song/iogg.mp3').play()
 }
 
 function maevSay(){
-  new Audio('/song/maev.mp3').play()
+  new Audio('./song/maev.mp3').play()
 }
 
 function gromSay(){
-  new Audio('/song/grom.mp3').play()
+  new Audio('./song/grom.mp3').play()
 }
 
 function jainaSay(){
-  new Audio('/song/jaina.mp3').play()
+  new Audio('./song/jaina.mp3').play()
 }
 
 function uterSay(){
-  new Audio('/song/uter.mp3').play()
+  new Audio('./song/uter.mp3').play()
 }
 
 function artasSay(){
-  new Audio('/song/artas.mp3').play()
+  new Audio('./song/artas.mp3').play()
 }
 function artasSay2(){
-  new Audio('/song/artas2.mp3').play()
+  new Audio('./song/artas2.mp3').play()
 }
 
 function nerzulSay(){
-  new Audio('/song/nerzul.mp3').play()
+  new Audio('./song/nerzul.mp3').play()
 }
 function nerzulSay2(){
-  new Audio('/song/nerzul2.mp3').play()
+  new Audio('./song/nerzul2.mp3').play()
 }
 
 function sargerasSay(){
-  new Audio('/song/sargeras.mp3').play()
+  new Audio('./song/sargeras.mp3').play()
 }
 
 function guldanSay(){
-  new Audio('/song/guldan.mp3').play()
+  new Audio('./song/guldan.mp3').play()
 }
 
 function medivSay(){
-  new Audio('/song/mediv.mp3').play()
+  new Audio('./song/mediv.mp3').play()
 }
 function kadgarSay(){
-  new Audio('/song/kadgar.mp3').play()
+  new Audio('./song/kadgar.mp3').play()
 }
